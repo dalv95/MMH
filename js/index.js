11447c9c4e4e5788536bf3f03e547b8be0f8c811
dalv95 <dalv95@gmail.com>
2020-12-22 19:06:19 +0100

    Create

---
+++ js/index.js
@@ -0,0 +1,61 @@
+$(function () {
+    $("#check_date").datepicker();
+    $("#check_date").datepicker();
+});
+
+$(function () {
+    $("#result_date").datepicker();
+    $("#result_date").datepicker();
+});
+
+$(document).ready(function () {
+    $("#start").click(function () {
+        var car1_max = 1000;
+        var car2_max = 1000;
+        var car3_max = 1000;
+        var car4_max = 1000;
+        var car5_max = 1000;
+        var car1_route;
+        var car2_route;
+        var car3_route;
+        var car4_route;
+        var car5_route;
+
+
+
+        var bialystok_need = 500;
+        var bielskobiala_need = 50;
+        var chrzanow_need = 400;
+        var gdansk_need = 200;
+        var gdynia_need = 100;
+        var gliwice_need = 40;
+        var gromnik_need = 200;
+        var katowice_need = 300;
+        var kielce_need = 30;
+        var krosno_need = 60;
+        var krynica_need = 50;
+        var lublin_need = 60;
+        var lodz_need = 160;
+        var malbork_need = 100;
+        var nowytarg_need = 120;
+        var olsztyn_need = 300;
+        var poznan_need = 100;
+        var pulawy_need = 200;
+        var radom_need = 100;
+        var rzeszow_need = 60;
+        var sandomierz_need = 200;
+        var szczecin_need = 150;
+        var szczucin_need = 60;
+        var szklarskaporeba_need = 50;
+        var tarnow_need = 70;
+        var warszawa_need = 200;
+        var wieliczka_need = 90;
+        var wroclaw_need = 40;
+        var zakopane_need = 200;
+        var zamosc_need = 300;
+
+
+
+        document.getElementById("car1").innerHTML = result;
+    });
+});
---
+++ js/maps.js
@@ -0,0 +1,54 @@
+// Initialize and add the map
+function initMap() {
+
+  var center  = {lat: 52.222384, lng: 19.3010533};
+
+  var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 6, center: center});
+
+  }
+
+function listPlace() {
+    var krakow = {lat: 50.0469018, lng: 19.8647886};
+    var bialystok = {lat: 53.1277077, lng: 23.0860259};
+    var bielskobiala = {lat: 49.8123373, lng: 18.8971777};
+    var chrzanow = {lat: 50.12887, lng: 19.2886585};
+    var gdansk = {lat: 54.3612063, lng: 18.5499434};
+    var gdynia = {lat: 54.5039433, lng: 18.3233953};
+    var gliwice = {lat: 50.3013748, lng: 18.5095319};
+    var gromnik = {lat: 49.8390505, lng: 20.9451044};
+    var katowice = {lat: 50.2138079, lng: 18.8671082};
+    var kielce = {lat: 50.8541274, lng: 20.5456013};
+    var krosno = {lat: 49.6897447, lng: 21.6817235};
+    var krynica = {lat: 49.4155437, lng: 20.86402};
+    var lublin = {lat: 50.064021, lng: 22.4236859};
+    var lodz = {lat: 51.7732471, lng: 19.3405084};
+    var malbork = {lat: 54.0286986, lng: 19.0084414};
+    var nowytarg = {lat: 49.4893579, lng: 19.9737204};
+    var olsztyn = {lat: 53.7760917, lng: 20.3956589};
+    var poznan = {lat: 52.4006553, lng: 16.7615818};
+    var pulawy = {lat: 51.4256066, lng: 21.9046277};
+    var radom = {lat: 51.4151517, lng: 21.0839339};
+    var rzeszow = {lat: 50.0055663, lng: 21.8483705};
+    var sandomierz = {lat: 50.064021, lng: 21.6755716};
+    var szczecin = {lat: 53.4298189, lng: 14.4845397};
+    var szczucin = {lat: 50.3096603, lng: 21.0617624};
+    var szklarskaporeba = {lat: 50.814363, lng: 15.3965225};
+    var tarnow = {lat: 50.026233, lng: 20.906866};
+    var warszawa = {lat: 52.2330269, lng: 20.7810095};
+    var wieliczka = {lat: 49.9876079, lng: 20.0110902};
+    var wroclaw = {lat: 51.1272097, lng: 16.8517796};
+    var zakopane = {lat: 49.2759821, lng: 19.9038356};
+    var zamosc = {lat: 50.7214316, lng: 23.2134931};
+
+    var x = document.getElementById("place").value;
+
+    
+    
+      if (x === 'krakow') {
+      var marker = new google.maps.Marker({ position: rydygiera, map: map });
+    }
+
+}
+  
+
